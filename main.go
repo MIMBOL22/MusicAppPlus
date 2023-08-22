b79e4b63cc2996412003a5495edaef7fe0839cb5
MIMBOL228 <aekemerov@yandex.ru>
2023-08-23 01:54:04 +0500

    INIT

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"embed"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "MusicAppPlus",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 205},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			BackdropType:         3,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
